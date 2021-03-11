f2197e4d66475f08dbad453be6d1c31a2d13e6e6
krevetka87 <bonanast4sia@yandex.ru>
2021-03-11 23:54:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports =
+	function toReadable(number) {
+		let one = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+		let ten = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+		let reminder;
+		let round;
+		if (number === 0) {
+			return 'zero';
+		}
+		if (number < 20) {
+			return one[number];
+		}
+		if (number >= 20 && number < 100) {
+			reminder = number % 10;
+			round = Math.floor(number / 10);
+			return (ten[round] + ' ' + one[reminder]).trim();
+		}
+		if (number >= 100) {
+			reminder = number % 100;
+			round = Math.floor(number / 100);
+			if (reminder < 20) {
+				return (`${one[round]} hundred ${one[reminder]}`).trim();
+			}
+			else {
+				newReminder = reminder % 10;
+				newRound = Math.floor(reminder / 10);
+				return (`${one[round]} hundred ${ten[newRound]} ${one[newReminder]}`).trim();
+			}
+
+		}
+	}
